b232209e9a174071dd05e64297024c7ce0eb6927
José Venancio Mosquera <venancio.mosquera@gmail.com>
2021-08-18 17:31:36 -0300

    Aula 164 - Whatsapp Udemy - Config Inicial (100%)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp_flutter/Pages/HomePage.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  firestore.collection("Users").doc("001").set({"name": "Venancio"});
+
   runApp(MaterialApp(
     home: HomePage(),
     debugShowCheckedModeBanner: false,
