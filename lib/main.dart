4782d4d24727f977aaed2458629e80a284152839
José Venancio Mosquera <venancio.mosquera@gmail.com>
2021-08-18 17:05:50 -0300

    Aula 164 - Whatsapp Udemy - Config Inicial

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_flutter/Pages/HomePage.dart';
+
+void main() async {
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
